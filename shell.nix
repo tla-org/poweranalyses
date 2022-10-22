c47e71012e2c6f3d090e82b721f79e102d5d2483
rikhuijzer <rikhuijzer@pm.me>
2022-10-22 11:46:30 +0200

    Remove nix shells

--- shell.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> {};
-
-let
-    # stable = <nixos-22.05> {};
-    stable = import (fetchTarball https://nixos.org/channels/nixos-22.05/nixexprs.tar.xz) {};
-in stable.clangStdenv.mkDerivation {
-  name = "clang-nix-shell";
-  buildInputs = [ ];
-}
