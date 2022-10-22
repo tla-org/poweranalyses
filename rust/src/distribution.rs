6d5e507638af1628403f783ca4dd1cbaf687d8e4
rikhuijzer <rikhuijzer@pm.me>
2022-10-22 17:44:06 +0200

    Simplify

--- rust/src/distribution.rs
+++ rust/src/distribution.rs
@@ -12,7 +12,7 @@ struct Normal {
 
 impl Distribution for Normal {
     fn pdf(&self, x: f64) -> f64 {
-        let frac: f64 = 1.0 / (self.sigma * (2.0 * PI).sqrt());
+        let frac = 1.0 / (self.sigma * (2.0 * PI).sqrt());
         let expable = -0.5 * ((x - self.mu) / self.sigma).powf(2.0);
         return frac * expable.exp();
     }
@@ -24,7 +24,6 @@ mod distributions {
 
     #[test]
     fn it_gives_pdf() {
-        let normal = Normal{mu: 1.0, sigma: 2.0};
-        assert_eq!(normal.pdf(2.2), 0.16661230144589984);
+        assert_eq!(Normal{mu: 1.0, sigma: 2.0}.pdf(2.2), 0.16661230144589984);
     }
 }
