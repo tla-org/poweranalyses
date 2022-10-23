829411ef8a493f21e163fd34b9351bb14022d321
rikhuijzer <rikhuijzer@pm.me>
2022-10-23 09:30:27 +0200

    Add `pnorm`

--- rust/src/rmath.rs
+++ rust/src/rmath.rs
@@ -4,6 +4,11 @@
 /// - is an opportunity for learning,
 /// - is more flexible when building to different platforms.
 
+use statrs::distribution::Normal;
+use statrs::distribution::ContinuousCDF;
+use statrs::function::gamma::ln_gamma;
+use statrs::function::beta::beta_inc;
+
 const DBL_EPSILON: f64 = 2.220446049250313e-16; // 2^-52
 const M_LOG10_2: f64 = 0.3010299956639812; // log(10, 2)
 const XMAX: f64 = 2.5327372760800758e305;
@@ -12,27 +17,20 @@ fn fmod(a: f64, b: f64) -> f64 {
     return a % b;
 }
 
-/// https://github.com/wch/r-source/blob/trunk/src/nmath/lgamma.c
 fn lgammafn(x: f64) -> f64 {
-    let mut sgn: i32 = 1;
-
-    if x == f64::NAN {
-        return x;
-    }
-
-    if x < 0. && fmod(f64::floor(-x), 2.) == 0. {
-        sgn = -1;
-    }
-
-    1.0
+    return ln_gamma(x);
 }
 
+/// Incomplete beta function.
 fn pbeta(x: f64, a: f64, b: f64) -> f64 {
-    1.0
+    // Not completely sure, maybe checked_beta_inc or something.
+    return beta_inc(x, a, b);
 }
 
+/// Normal (cumulative) distribution function.
 fn pnorm(x: f64) -> f64 {
-    1.0
+    let unit_normal = Normal::new(0.0, 1.0).unwrap();
+    return unit_normal.cdf(x);
 }
 
 fn pnt(t: f64, df: f64, ncp: f64, lower_tail: i32, log_p: i32) -> f64 {
@@ -42,11 +40,14 @@ fn pnt(t: f64, df: f64, ncp: f64, lower_tail: i32, log_p: i32) -> f64 {
 #[cfg(test)]
 mod rmath_tests {
     extern crate approx;
+    use approx::assert_ulps_eq;
 
     use super::*;
 
     #[test]
-    fn it_gives_remainder() {
-        approx::assert_ulps_eq!(1.8, fmod(9.2, 3.7), max_ulps = 6);
+    fn equalities() {
+        assert_ulps_eq!(1.8, fmod(9.2, 3.7), max_ulps = 6);
+
+        assert_eq!(0.9986501019684255, pnorm(3.));
     }
 }
