39176e20c8159d81cc4c6218108ffa053553d9fc
rikhuijzer <rikhuijzer@pm.me>
2022-10-28 14:37:58 +0200

    Implement `alpha`

--- src/power.rs
+++ src/power.rs
@@ -9,9 +9,15 @@ trait StatisticalTest {
         let d1 = self.alternative_distribution();
         let right_tail = if self.tail() == 1 { alpha } else { alpha / 2.0 };
         let critical_value = d0.quantile(right_tail, false);
-        // maybe reverse; also previous line.
         let beta = d1.cdf(critical_value, false);
         return beta;
+    }
+    fn alpha(&self, power: f64) -> f64 {
+        let d0 = self.null_distribution();
+        let d1 = self.alternative_distribution();
+        let critical_value = d1.quantile(power, false);
+        let right_tail = d0.cdf(critical_value, false);
+        return if self.tail() == 1 { right_tail } else { 2.0 * right_tail };
     }
 }
 
@@ -79,6 +85,8 @@ mod tests {
         let n = 50.0;
         assert_eq!(OneSampleTTest::new(1, n, es).power(alpha), 0.9672067458263426);
         assert_eq!(OneSampleTTest::new(2, n, es).power(alpha), 0.9338975528614741);
+        assert_eq!(OneSampleTTest::new(1, n, es).alpha(power), 0.03365841504997329);
+        assert_eq!(OneSampleTTest::new(2, n, es).alpha(power), 0.06731683009994659);
     }
 }
 
