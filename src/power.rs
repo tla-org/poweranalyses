ee3033d49a322337fa13894f1f8a4c0b3fad6d61
rikhuijzer <rikhuijzer@pm.me>
2022-10-28 15:46:49 +0200

    Got `es` root finding working

--- src/power.rs
+++ src/power.rs
@@ -1,5 +1,7 @@
 use crate::dist::*;
 
+use roots::*;
+
 trait StatisticalTest {
     fn null_distribution(&self, es: f64, n: f64) -> Box<dyn Distribution>;
     fn alternative_distribution(&self, es: f64, n: f64) -> Box<dyn Distribution>;
@@ -18,6 +20,16 @@ trait StatisticalTest {
         let critical_value = d1.quantile(power, false);
         let right_tail = d0.cdf(critical_value, false);
         return if self.tail() == 1 { right_tail } else { 2.0 * right_tail };
+    }
+    fn es(&self, alpha: f64, power: f64, n: f64) -> f64 {
+        let f = | es | { self.alpha(es, power, n) - alpha };
+        let mut convergency = SimpleConvergency { eps: 1e-15f64, max_iter: 100 };
+        return find_root_brent(0f64, 1000f64, &f, &mut convergency).unwrap();
+    }
+    fn n(&self, alpha: f64, power: f64, es: f64) -> f64 {
+        let f = | n | { self.alpha(es, power, n) - alpha };
+        let mut convergency = SimpleConvergency { eps: 1e-15f64, max_iter: 100 };
+        return find_root_brent(-1000f64, 1000f64, &f, &mut convergency).unwrap();
     }
 }
 
@@ -53,10 +65,14 @@ mod tests {
         let alpha = 0.05;
         let power = 0.95;
         let n = 50.0;
-        assert_eq!(OneSampleTTest::new(1).power(es, alpha, n), 0.9672067458263426);
-        assert_eq!(OneSampleTTest::new(2).power(es, alpha, n), 0.9338975528614741);
-        assert_eq!(OneSampleTTest::new(1).alpha(es, power, n), 0.03365841504997329);
         assert_eq!(OneSampleTTest::new(2).alpha(es, power, n), 0.06731683009994659);
+        assert_eq!(OneSampleTTest::new(1).alpha(es, power, n), 0.03365841504997329);
+        assert_eq!(OneSampleTTest::new(2).power(es, alpha, n), 0.9338975528614741);
+        assert_eq!(OneSampleTTest::new(1).power(es, alpha, n), 0.9672067458263426);
+
+        assert_eq!(OneSampleTTest::new(2).es(alpha, power, n), 0.5201211596125199);
+        assert_eq!(OneSampleTTest::new(1).es(alpha, power, n), 0.4718256927232961);
+        // assert_eq!(OneSampleTTest::new(2).n(alpha, power, es), 0.5201211596125199);
     }
 }
 
