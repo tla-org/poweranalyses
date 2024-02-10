e3a7b9ef8c0d29c0b8d0d70c2df59db660750fad
Jose Storopoli <jose@storopoli.io>
2024-02-10 15:10:29 +0000

    feat: dioxus template

---
+++ app/src/main.rs
@@ -0,0 +1,24 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+
+#[cfg(feature = "debug")]
+use log::{info, LevelFilter};
+
+fn main() {
+    // launch the web app
+    dioxus_web::launch(App);
+}
+
+// create a component that renders a div with the text "Hello, world!"
+fn App(cx: Scope) -> Element {
+    #[cfg(feature = "debug")]
+    {
+        // init logger for Dioxus
+        dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
+    }
+    // launch the web app
+    #[cfg(feature = "debug")]
+    info!("Launching PowerAnalyses app");
+    cx.render(rsx! { div { "Hello, world!" } })
+}
