a18e3256a33a2ed8c707f9776599b2a22006fbef
Jose Storopoli <jose@storopoli.io>
2024-02-28 21:11:36 -0300

    feat: svelte + wasm works

---
+++ app/src/App.svelte
@@ -0,0 +1,46 @@
+<script context="module">
+    function writeToPtr(ptr, text) {
+        const buffer = Module.HEAPU8.buffer;
+        const view = new Uint8Array(buffer, ptr, 1024);
+        const encoder = new TextEncoder();
+        const with_stop = text + "<END>";
+        view.set(encoder.encode(with_stop));
+    }
+
+    function readFromPtr(ptr) {
+        const buffer = Module.HEAPU8.buffer;
+        const view = new Uint8Array(buffer, ptr, 1024);
+        const length = view.findIndex(byte => byte === 0);
+        const decoder = new TextDecoder();
+
+        return decoder.decode(new Uint8Array(buffer, ptr, length));
+    }
+
+    /** Update the output area by calculating the numbers via WebAssembly. */
+    function getOutput() {
+        const state = {"analysis":"n","test":"ANCOVA","n":107,"alpha":0.05,"power":0.95,"es":0.5,"q":"10","k":"5","p":"2"};
+        const json = JSON.stringify(state);
+        console.log(`Sending the following json to the back end: ${json}`);
+
+        const ptr = Module._alloc();
+        writeToPtr(ptr, json);
+        Module._calculatePower(ptr);
+        const returned = readFromPtr(ptr);
+        Module._dealloc(ptr);
+        console.log(`Received the following json from the back end: ${returned}`);
+        const result = JSON.parse(returned);
+        const id = Object.keys(result)[0];
+
+        return result;
+    }
+</script>
+
+<div class="content">
+    <div class="title center">
+        <img class="favicon" src="/src/assets/favicon.png" alt="favicon" />
+          PowerAnalyses.org Beta
+    </div>
+    Got the following response from the back end: {getOutput().n}
+</div>
+
+
--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,15 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-    kit: {
-        adapter: adapter({
-            fallback: '404.html'
-        }),
-        paths: {
-            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-        }
-    }
-};
-
-export default config;
+export default {
+  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+}
